c90fbcd3d3fc20203c198992ec19378361ac538c
Metin Şaylan <metinsaylan@gmail.com>
2020-05-09 16:00:21 +0300

    update spaces

--- header.php
+++ header.php
@@ -1,12 +1,10 @@
 <!doctype html>
 <html lang="en">
 <head>
-	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1">
-	
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1">
 <?php wp_head(); ?>
-
 </head>
 <body>
 <div class="wrap">
